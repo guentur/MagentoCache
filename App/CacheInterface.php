5a130e3f592970b6d4776579f39b8ff4f4c18109
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-03-17 12:42:52 +0200

    added module cache interface

---
+++ App/CacheInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Guentur\CacheWrapper\App;
+
+use Magento\Framework\App\CacheInterface as FrameworkCacheInterface;
+
+interface CacheInterface extends FrameworkCacheInterface
+{
+    /**
+     * @param string $index
+     * @param callable $data
+     * @param array $cacheTags
+     * @return mixed
+     */
+    public function getCached(string $index, callable $data, array $cacheTags = []);
+
+    /**
+     * @param string $index
+     * @return mixed
+     */
+    public function getDataFromCache(string $index);
+
+    /**
+     * @param string $index
+     * @param array $data
+     * @param array $cacheTags
+     * @return mixed
+     */
+    public function saveToCache(string $index, array $data, array $cacheTags = []);
+
+    /**
+     * @param string $mode
+     * @param array $tags
+     * @return mixed
+     */
+    public function cleanWithMode(string $mode, array $tags = []);
+}
